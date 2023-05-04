baf0a72ff99b08aa6b633fcf49d7216e0cf09ee1
juanramirez18 <juanramirez189@hotmail.com>
2023-05-04 16:09:24 -0500

    seven commit

--- pedido-restaurante/src/index.ts
+++ pedido-restaurante/src/index.ts
@@ -3,10 +3,32 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  // Connect the client
-  await prisma.$connect()
+    try {
+        // Connect the client
+        await prisma.$connect()
   // ... you will write your Prisma Client queries here
-  console.log("Se ha conectado exitosamente a la BD")
+        console.log("Se ha conectado exitosamente a la BD")
+
+  //Crear usuario en BD
+  const createUser = await prisma.user.create({
+    data:{
+        name: "juan carlos",
+        lastName: "Ramirez lopez",
+        email: "juanramirez1@hotmail.com"
+
+    }
+
+  })
+
+  //listar usuarios en al BD
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers)
+        
+    } catch (error) {
+        throw new Error(JSON.stringify(error))
+        
+    }
+  
 }
 
 main()
